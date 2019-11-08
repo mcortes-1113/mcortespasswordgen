e3c095ddad7fbdf657f4a72b1184abc00aa07d66
Mario Cortes <mcortes.1113@gmail.com>
2019-11-08 00:03:40 -0600

    Replaced previous password generator function with new one to include all character types on character pool. Non-functioning status. Need to debug. Input validation pending

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
-var passLength = document.getElementById("usrInputLength");
+var passLength = document.getElementById("usrLength");
+var btnEl = document.getElementById("btn");
+var passChPool = [];
 var passValues = [];
 var password;
-var passwordNode;
 
-/*var ElNumCk = document.querySelector("#numeric");
-
-ElNumCk.setAttribute(false);*/
+btnEl.addEventListener('click', genPass());
 
 function numCheck(cb) {
   if (cb.checked === true) {
-      passValues.push(1,2,3,4,5,6,7,8,9,0);
-      console.log(passValues);
+      passChPool.push(1,2,3,4,5,6,7,8,9,0);
   }
 }
   function lwCheck(cb) {
     if (cb.checked === true) {
-        passValues.push("a","b","c","d");
-        console.log(passValues);
+        passChPool.push("a","b","c","d");
     }
   }
     function upCheck(cb) {
         if (cb.checked === true) {
-            passValues.push("A","B","C","D");
-            console.log(passValues);
+            passChPool.push("A","B","C","D");
         }
     }
         function spCheck(cb) {
             if (cb.checked === true) {
-                passValues.push("\@","\#","\!","\%");
-                console.log(passValues);
+                passChPool.push("\@","\#","\!","\%");
             }
         }
+
+            function genPass(){
+                for (var i = 0;i <= passLength-1;i++) {
+                    var rand = passChPool([Math.floor(Math.random()*passChPool.length)]);
+                    passValues.push(rand);
+                }
+//Need to fix issue with array value not being assigned to variable
+                    return passValues;
+                    alert("the password is ");
+                    alert(passValues);
+            }
+                
